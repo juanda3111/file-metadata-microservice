6121af883b4c010f0acb969026f019769a23011a
juanda3111 <judarogu1@gmail.com>
2019-11-27 18:08:45 -0500

    subo el file metadata microservice

---
+++ app.js
@@ -0,0 +1,26 @@
+//imports de JS
+var express =require('express');
+var bodyParser= require('body-parser');
+var cors = require('cors');
+var multer = require('multer');
+
+var upload = multer({dest:'upload/'});
+
+var app = module.exports = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.use(express.static(__dirname + '/public'));
+
+
+app.post('/upload', upload.single('file'), function(req, res, next){
+
+    return res.json(req.file);
+});
+
+
+
+app.listen(5000, function() {
+    console.log("funciona");
+})
